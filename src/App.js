e3547550304c4a5d454c8915d0c7140a2bd7a492
Daniel Chiquete <dchiquete2019@gmail.com>
2024-05-09 14:28:27 -0700

    new changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <Router>
       <div className='w-full h-screen flex flex-nowrap'>
-        <div className='inline-flex flex-col min-w-72 max-w-md border-box p-1.5 gap-1.5'> {/* Left Half - Static */}
+        <div className='inline-flex flex-col min-w-72 max-w-md border-box p-1.5 gap-1.5 fixed'> {/* Left Half - Static */}
           <ul className='w-100 flex flex-col bg-secondary rounded-lg bg-secondary border-box p-4 gap-y-4'>
           
             <Link to="/dashboard" className='inline-flex max-w-max'>
@@ -47,8 +47,25 @@ function App() {
                 </button>
               </li>
             </div>
-            <div className='flex w-full bg-teritary h-32 rounded-lg'>
-              Hello
+            <div className='pb-20 flex flex-col gap-y-4'>
+              <section className='flex w-full flex-col bg-neutral-800 rounded-lg border-box p-4 gap-y-5'>
+                <div>
+                  <p className='text-base font-medium'>Create your first playlist</p>
+                  <p className='text-sm font-medium'>It's easy, we'll help you</p>
+                </div>
+                <button className='inline-flex rounded-full max-w-max bg-white items-center justify-center px-4 py-1 hover:scale-105 hover:opacity-90'>
+                  <p className='text-sm font-medium text-black'>Create Playlist</p>
+                </button>
+              </section>
+              <section className='flex w-full flex-col bg-neutral-800 rounded-lg border-box p-4 gap-y-5'>
+                <div>
+                  <p className='text-base font-medium'>Let's find some podcasts to follow</p>
+                  <p className='text-sm font-medium'>We'll keep you updated on new episodes</p>
+                </div>
+                <button className='inline-flex rounded-full max-w-max bg-white items-center justify-center px-4 py-1 hover:scale-105 hover:opacity-90'>
+                  <p className='text-sm font-medium text-black'>Browse podcasts</p>
+                </button>
+              </section>
             </div>
           </ul>
         </div>
